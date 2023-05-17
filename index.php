cdc8811f30a77ead36ae3aacc76e6cb491499eda
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-05-17 15:18:18 +0200

    add index db and models

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+include __DIR__ . '/db.php';
+
+//var_dump($products);
+?>
+
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>php opp-2</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+
+    <header>
+        <nav class="d-flex align-items-cente justify-content-between shadow px-4 ">
+            <div class="img-logo">
+                <img class="img-fluid" src="./img/hand-drawn-dog-animal-logo_23-2148910632.avif" alt="logo cane">
+
+            </div>
+        </nav>
+    </header>
+
+</body>
+
+</html>
